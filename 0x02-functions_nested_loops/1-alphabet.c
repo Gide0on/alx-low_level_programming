d1606fddfbb7d735c823e7395f7c4a377a64b482
Gide0on <gideonolayemi976@gmail.com>
2022-04-12 08:16:43 -0700

    My commits

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
