6fb59c473af36cefab6b4124aea447a91442382c
Gideon Ateji <101395299+Gide0on@users.noreply.github.com>
2022-04-21 05:39:03 +0100

    1 commits

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  * swap_int - swaps the values of two integers
- *   * @a: pointer 1
- *    * @b: pointer 2
- *     * Return: void
- *      */
+ * swap_int - swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+ */
 
 void swap_int(int *a, int *b)
 {
-		int x = *a;
-			*a = *b;
-				*b = x;
+	int x = *a;
+	*a = *b;
+	*b = x;
 }
