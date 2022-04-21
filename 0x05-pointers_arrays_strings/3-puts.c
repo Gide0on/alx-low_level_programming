cb49e4e3248f46af3a6681324880b9ce1e18c59d
Gideon Ateji <101395299+Gide0on@users.noreply.github.com>
2022-04-21 05:48:22 +0100

    3 commits

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,21 @@
 
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
